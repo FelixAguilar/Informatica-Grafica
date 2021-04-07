7cd59accd3dcb8ddbf28a39e126feadbeda3789f
APujol97 <pujolvillegas@gmail.com>
2021-04-07 15:13:23 +0200

    cambio en proceso

--- Etapa_2/Etapa_2_pendulo.cpp
+++ Etapa_2/Etapa_2_pendulo.cpp
@@ -3,23 +3,32 @@
 ////////////////////////////////////////////////////
 #include <GL/freeglut.h>
 #include <math.h>
+#include <windows.h>
 //#define _USE_MARH_DEFINES
 
+const GLfloat l = 0.52f;
+const GLfloat m1 = 1.0f;
+const GLfloat m2 = 1.0f;
+const GLfloat g = 9.8f;
+const GLint polygon_points = 360;
+const GLfloat radius = 0.025f;
 const GLfloat pi = M_PI;
-const GLint W_WIDTH = 1500; // Tamaño incial de la ventana
-const GLint W_HEIGHT = 1500;
+const GLint W_WIDTH = 1200; // Tamaño incial de la ventana
+const GLint W_HEIGHT = 1200;
 const GLint W_RATIO = W_WIDTH/W_HEIGHT;
 GLint initiated = 0;
-GLfloat fAngulo = 120.0f; // Variable que indica el ángulo de rotación de los ejes. 
+GLfloat fAngulo1 = 120.0f; // Variable que indica el ángulo de rotación de los ejes. 
 GLfloat anglePrevi = 0.0f;
-GLfloat fAngulo2 = 0.0;
+GLfloat fAngulo2 = 130.0f;
 GLfloat angleTool;//Variable para construir polígonos mediante ángulos;  
 //= 2*pi/polygon_points 
 GLboolean sentido_horario1 = GL_FALSE;
 GLboolean sentido_horario2 = GL_TRUE;
 GLint tiempo_1 = 10;
-GLint tiempo_2 = 10;
-GLfloat xp, yp;
+
+SYSTEMTIME st;
+GetSystemTime(&st);
+GLfloat tiempo_init = st.wMinute * 1000 + st.wMilliseconds;
 // Función que visualiza la escena OpenGL
 GLfloat toRadians(GLfloat i)
 {
@@ -29,8 +38,6 @@ GLfloat toRadians(GLfloat i)
 
 void drawScene(){
 
-	GLint polygon_points = 360;
-	GLfloat radius = 0.025f;
 	GLfloat angleTool = 0.0f;
 
 	// Dibujo del péndulo 2
@@ -54,78 +61,6 @@ void drawScene(){
 
 	// glPopMatrix();
 
-	//SEGUNDO PÉNDULO
-
-	glPushMatrix();
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-	glTranslatef(sin(toRadians(fAngulo))*0.52, 0.52 - cos(toRadians(fAngulo))*0.52, 0.0f);
-	glRotatef(fAngulo2, 0.0f, 0.0f, 1.0f);
-
-	xp = cos(toRadians(fAngulo))*-0.025f - sin(toRadians(fAngulo))*0.52f;
-	yp = cos(toRadians(fAngulo))*0.52f + sin(toRadians(fAngulo))*-0.025f;
-
-	glBegin(GL_POLYGON);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex2f(-0.025f, 0.00f);
-	glVertex2f(0.025f, 0.00f);
-	glVertex2f(0.025f, -0.52f);
-	glVertex2f(-0.025f, -0.52f);
-	glEnd();
-
-	glBegin(GL_POLYGON);
-	glColor3f(0.0f, 0.0f, 0.0f);
-    for (GLint i = 0; i < polygon_points; i++)
-    {
-        glVertex2d(0.0 + radius * cos(angleTool), -0.52 +radius * sin(angleTool));
-        angleTool += 2.0f * pi / polygon_points;
-    }
-    glEnd();
-
-	glPopMatrix();
-
-	//PRIMER PÉNDULO
-
-    glPushMatrix();
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-
-    glTranslatef(0.0f, 0.52f, 0.0f);
-	glRotatef(fAngulo, 0.0f, 0.0f, 1.0f);
-	glTranslatef(0.0f, -0.52f, 0.0f);
-
-	// Creamos a continuación dibujamos los poligonos
-	glBegin(GL_POLYGON);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex2f(-0.025f, 0.52f);
-	glVertex2f(0.025f, 0.52f);
-	glVertex2f(0.025f, 0.0f);
-	glVertex2f(-0.025f, 0.0f);
-	glEnd();
-
-	glBegin(GL_POLYGON);
-	glColor3f(0.0f, 0.0f, 1.0f);
-    for (GLint i = 0; i < polygon_points; i++)
-    {
-        glVertex2d(radius * cos(angleTool), radius * sin(angleTool));
-        angleTool += 2.0f * pi / polygon_points;
-    }
-    glEnd();
-
-	angleTool = 0.0f;
-
-	glBegin(GL_POLYGON);
-	glColor3f(0.0f, 0.0f, 0.0f);
-    for (GLint i = 0; i < polygon_points; i++)
-    {
-        glVertex2d(0.0 + radius * cos(angleTool), 0.52 +radius * sin(angleTool));
-        angleTool += 2.0f * pi / polygon_points;
-    }
-    glEnd();
-
-	glPopMatrix();
-
 	// Dibujamos los ejes
 	glPushMatrix();
 
@@ -145,6 +80,76 @@ void drawScene(){
 
 	glPopMatrix();
 
+	//SEGUNDO PÉNDULO
+
+	glPushMatrix();
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glTranslatef(sin(toRadians(fAngulo1))*0.52, 0.52 - cos(toRadians(fAngulo1))*0.52, 0.0f);
+	glRotatef(fAngulo2, 0.0f, 0.0f, 1.0f);
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.025f, 0.00f);
+	glVertex2f(0.025f, 0.00f);
+	glVertex2f(0.025f, -0.52f);
+	glVertex2f(-0.025f, -0.52f);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	angleTool = 0.0f;
+    for (GLint i = 0; i < polygon_points; i++)
+    {
+        glVertex2d(0.0 + radius * cos(angleTool), -0.52 +radius * sin(angleTool));
+        angleTool += 2.0f * pi / polygon_points;
+    }
+    glEnd();
+
+	glPopMatrix();
+
+	//PRIMER PÉNDULO
+
+    glPushMatrix();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glTranslatef(0.0f, 0.52f, 0.0f);
+	glRotatef(fAngulo1, 0.0f, 0.0f, 1.0f);
+	glTranslatef(0.0f, -0.52f, 0.0f);
+
+	// Creamos a continuación dibujamos los poligonos
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-0.025f, 0.52f);
+	glVertex2f(0.025f, 0.52f);
+	glVertex2f(0.025f, 0.0f);
+	glVertex2f(-0.025f, 0.0f);
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	angleTool = 0.0f;
+    for (GLint i = 0; i < polygon_points; i++)
+    {
+        glVertex2d(radius * cos(angleTool), radius * sin(angleTool));
+        angleTool += 2.0f * pi / polygon_points;
+    }
+    glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	angleTool = 0.0f;
+    for (GLint i = 0; i < polygon_points; i++)
+    {
+        glVertex2d(0.0 + radius * cos(angleTool), 0.52 + radius * sin(angleTool));
+        angleTool += 2.0f * pi / polygon_points;
+    }
+    glEnd();
+
+	glPopMatrix();
+
 }
 
 void Display(void)
@@ -161,29 +166,40 @@ void Display(void)
 
 void Timer(GLint t)
 {
-	// Control Angulo1
-	if (fAngulo <= -120)
-		sentido_horario1 = GL_TRUE;
-	if (fAngulo >= 120)
-		sentido_horario1 = GL_FALSE;
-	if (sentido_horario1) {
-		fAngulo += 3.0f;
-	}
-	else {
-		fAngulo -= 3.0f;
-	}
 
-	// Control Angulo2
-	if (fAngulo2 <= -120)
-		sentido_horario2 = GL_TRUE;
-	if (fAngulo2 >= 120)
-		sentido_horario2 = GL_FALSE;
-	if (sentido_horario2) {
-		fAngulo2 += 6.0f;
-	}
-	else {
-		fAngulo2 -= 6.0f;
-	}
+	GetSystemTime(&st);
+
+	GLfloat tiempo_elapsed = tiempo_init - (st.wMinute + 1000 + st.wMilliseconds);
+	tiempo_init = st.wMinute + 1000 + st.wMilliseconds;
+
+	GLfloat vel_ang_1 = 0/tiempo_elapsed;
+	GLfloat vel_ang_2 = 0/tiempo_elapsed;
+
+	fAngulo1 = (-g * (2 * m1 + m2) * sin(fAngulo1) - m2 * g * sin(fAngulo1 - 2 * fAngulo2) - 2 * sin(fAngulo1 - fAngulo2) * m2 * (fAngulo2))
+
+	// Control Angulo1
+	// if (fAngulo1 <= -120)
+	// 	sentido_horario1 = GL_TRUE;
+	// if (fAngulo1 >= 120)
+	// 	sentido_horario1 = GL_FALSE;
+	// if (sentido_horario1) {
+	// 	fAngulo1 += 3.0f;
+	// }
+	// else {
+	// 	fAngulo1 -= 3.0f;
+	// }
+
+	// // Control Angulo2
+	// if (fAngulo2 <= -120)
+	// 	sentido_horario2 = GL_TRUE;
+	// if (fAngulo2 >= 120)
+	// 	sentido_horario2 = GL_FALSE;
+	// if (sentido_horario2) {
+	// 	fAngulo2 += 6.0f;
+	// }
+	// else {
+	// 	fAngulo2 -= 6.0f;
+	// }
 
 	// Indicamos que es necesario repintar la pantalla
 	glutPostRedisplay();
