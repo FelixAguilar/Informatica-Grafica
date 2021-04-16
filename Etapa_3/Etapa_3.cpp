5528c1f34caf3bf164675f1bbe50e535e1a5062e
Felix Aguilar Ferrer <aguilarferrerfelix@gmail.com>
2021-04-16 17:35:12 +0200

    Mano robot con rotación

--- Etapa_3/Etapa_3.cpp
+++ Etapa_3/Etapa_3.cpp
@@ -1,4 +1,4 @@
-// Etapa_3.cpp
+﻿// Etapa_3.cpp
 // Fichero principal.
 ////////////////////////////////////////////////////
 #include <GL/freeglut.h>
@@ -9,12 +9,13 @@ GLfloat fAngulo1 = 0.0f; //Subir y Bajar el Brazo.
 GLfloat fAngulo2 = 0.0f; //Subir y bajar Antebrazo.
 GLfloat fAngulo3 = 0.0f; // Subir y bajar Muñeca.
 GLfloat fAngulo4 = 0.0f; //Abrir y cerrar Mano.
+GLfloat fAngulo5 = 45.0f; //Giro de la Mano.
 
 // Constantes para dibujar los circulos.
 const GLfloat pi = M_PI;
 const GLint polygon_points = 360;
-const GLdouble radius_arm= 0.025d;
-const GLdouble radius_hand = 0.01d;
+const GLdouble radius_arm = 0.05;
+const GLdouble radius_hand = 0.025;
 
 //variables para el control de giro de los ángulos.
 const GLint tiempo = 10;
@@ -22,6 +23,7 @@ GLboolean sentido_horario1 = GL_FALSE;
 GLboolean sentido_horario2 = GL_TRUE;
 GLboolean sentido_horario3 = GL_TRUE;
 GLboolean sentido_horario4 = GL_FALSE;
+GLboolean sentido_horario5 = GL_FALSE;
 
 // Constantes de la ventana.
 const GLint W_WIDTH = 800;
@@ -39,269 +41,338 @@ GLfloat toRadians(GLfloat i)
 	return r;
 }
 
-void draw2DScene() {
+//void draw2DScene() {
+//
+//	GLfloat angleTool = 0.0f;
+//
+//	// Dibujamos los ejes.
+//	glPushMatrix();
+//
+//	glEnable(GL_LINE_SMOOTH);
+//	glBegin(GL_LINES);
+//	glLineWidth(1);
+//	glColor3f(0.0f, 0.0f, 0.0f);
+//
+//	glVertex2f(-1.0f, 0.0f);
+//	glVertex2f(1.0f, 0.0f);
+//
+//
+//	glVertex2f(0.0f, -1.0f);
+//	glVertex2f(0.0f, 1.0f);
+//
+//	glEnd();
+//
+//	glPopMatrix();
+//
+//	// Mano superior.
+//	glPushMatrix();
+//	glMatrixMode(GL_MODELVIEW);
+//	glLoadIdentity();
+//
+//	glTranslatef(
+//		-(0.52 - cos(toRadians(fAngulo1)) * 0.52) + (cos(toRadians(fAngulo2)) * 0.52),
+//		(sin(toRadians(fAngulo1)) * 0.52) + (sin(toRadians(fAngulo2)) * 0.52),
+//		0.0f);
+//
+//	glTranslatef(-0.8f + 0.52, -0.2f, 0.0f);
+//
+//	glRotatef(45.0f + fAngulo3 + fAngulo4, 0.0f, 0.0f, 1.0f);
+//	glTranslatef(0.0f, 0.01f, 0.0f);
+//
+//	glBegin(GL_POLYGON);
+//	glColor3f(1.0f, 0.0f, 0.0f);
+//	glVertex2f(0.12f, -0.01f);
+//	glVertex2f(0.12f, 0.01f);
+//	glVertex2f(0.0f, 0.01f);
+//	glVertex2f(0.0f, -0.01f);
+//	glEnd();
+//
+//	glTranslatef(0.12f, 0.0f, 0.0f);
+//	glRotatef(-90.0f, 0.0f, 0.0f, 1.0f);
+//
+//
+//	glBegin(GL_POLYGON);
+//	glColor3f(1.0f, 0.0f, 0.0f);
+//	glVertex2f(0.12f, -0.01f);
+//	glVertex2f(0.12f, 0.01f);
+//	glVertex2f(0.0f, 0.01f);
+//	glVertex2f(0.0f, -0.01f);
+//	glEnd();
+//
+//	glBegin(GL_POLYGON);
+//	glColor3f(0.0f, 0.0f, 0.0f);
+//	angleTool = 0.0f;
+//	for (GLint i = 0; i < polygon_points; i++)
+//	{
+//		glVertex2d(radius_hand * cos(angleTool), radius_hand * sin(angleTool));
+//		angleTool += 2.0f * pi / polygon_points;
+//	}
+//	glEnd();
+//
+//	glBegin(GL_POLYGON);
+//	glColor3f(0.0f, 0.0f, 0.0f);
+//	angleTool = 0.0f;
+//	for (GLint i = 0; i < polygon_points; i++)
+//	{
+//		glVertex2d(0.12f + radius_hand * cos(angleTool), radius_hand * sin(angleTool));
+//		angleTool += 2.0f * pi / polygon_points;
+//	}
+//	glEnd();
+//
+//	glPopMatrix();
+//
+//	// Mano inferior.
+//	glPushMatrix();
+//	glMatrixMode(GL_MODELVIEW);
+//	glLoadIdentity();
+//
+//	glTranslatef(
+//		-(0.52 - cos(toRadians(fAngulo1)) * 0.52) + (cos(toRadians(fAngulo2)) * 0.52),
+//		(sin(toRadians(fAngulo1)) * 0.52) + (sin(toRadians(fAngulo2)) * 0.52),
+//		0.0f);
+//
+//	glTranslatef(-0.8f + 0.52, -0.2f, 0.0f);
+//
+//	glRotatef(-45.0f + fAngulo3 - fAngulo4, 0.0f, 0.0f, 1.0f);
+//	glTranslatef(0.0f, -0.01f, 0.0f);
+//
+//	glBegin(GL_POLYGON);
+//	glColor3f(1.0f, 0.0f, 0.0f);
+//	glVertex2f(0.12f, -0.01f);
+//	glVertex2f(0.12f, 0.01f);
+//	glVertex2f(0.0f, 0.01f);
+//	glVertex2f(0.0f, -0.01f);
+//	glEnd();
+//
+//	glTranslatef(0.12f, 0.0f, 0.0f);
+//	glRotatef(90.0f, 0.0f, 0.0f, 1.0f);
+//
+//
+//	glBegin(GL_POLYGON);
+//	glColor3f(1.0f, 0.0f, 0.0f);
+//	glVertex2f(0.12f, -0.01f);
+//	glVertex2f(0.12f, 0.01f);
+//	glVertex2f(0.0f, 0.01f);
+//	glVertex2f(0.0f, -0.01f);
+//	glEnd();
+//
+//	glBegin(GL_POLYGON);
+//	glColor3f(0.0f, 0.0f, 0.0f);
+//	angleTool = 0.0f;
+//	for (GLint i = 0; i < polygon_points; i++)
+//	{
+//		glVertex2d(radius_hand * cos(angleTool), radius_hand * sin(angleTool));
+//		angleTool += 2.0f * pi / polygon_points;
+//	}
+//	glEnd();
+//
+//	glBegin(GL_POLYGON);
+//	glColor3f(0.0f, 0.0f, 0.0f);
+//	angleTool = 0.0f;
+//	for (GLint i = 0; i < polygon_points; i++)
+//	{
+//		glVertex2d(0.12f + radius_hand * cos(angleTool), radius_hand * sin(angleTool));
+//		angleTool += 2.0f * pi / polygon_points;
+//	}
+//	glEnd();
+//
+//	glPopMatrix();
+//
+//	// Antebrazo.
+//	glPushMatrix();
+//	glMatrixMode(GL_MODELVIEW);
+//	glLoadIdentity();
+//
+//	glTranslatef(-(0.52 - cos(toRadians(fAngulo1)) * 0.52), sin(toRadians(fAngulo1)) * 0.52, 0.0f);
+//
+//	glTranslatef(-0.8f + 0.52, -0.2f, 0.0f);
+//	glRotatef(fAngulo2, 0.0f, 0.0f, 1.0f);
+//
+//	glBegin(GL_POLYGON);
+//	glColor3f(1.0f, 0.0f, 0.0f);
+//	glVertex2f(0.52f, -0.025f);
+//	glVertex2f(0.52f, 0.025f);
+//	glVertex2f(0.0f, 0.025f);
+//	glVertex2f(0.0f, -0.025f);
+//	glEnd();
+//
+//	glBegin(GL_POLYGON);
+//	glColor3f(0.0f, 0.0f, 0.0f);
+//	angleTool = 0.0f;
+//	for (GLint i = 0; i < polygon_points; i++)
+//	{
+//		glVertex2d(0.52 + radius_arm * cos(angleTool), 0.0 + radius_arm * sin(angleTool));
+//		angleTool += 2.0f * pi / polygon_points;
+//	}
+//	glEnd();
+//
+//	glPopMatrix();
+//
+//	// Brazo.
+//
+//	glPushMatrix();
+//	glMatrixMode(GL_MODELVIEW);
+//	glLoadIdentity();
+//
+//	glTranslatef(-0.8f, -0.2f, 0.0f);
+//	glRotatef(fAngulo1, 0.0f, 0.0f, 1.0f);
+//
+//	glBegin(GL_POLYGON);
+//	glColor3f(1.0f, 0.0f, 0.0f);
+//	glVertex2f(0.52f, -0.025f);
+//	glVertex2f(0.52f, 0.025f);
+//	glVertex2f(0.0f, 0.025f);
+//	glVertex2f(0.0f, -0.025f);
+//	glEnd();
+//
+//	glBegin(GL_POLYGON);
+//	glColor3f(0.0f, 0.0f, 0.0f);
+//	angleTool = 0.0f;
+//	for (GLint i = 0; i < polygon_points; i++)
+//	{
+//		glVertex2d(radius_arm * cos(angleTool), radius_arm * sin(angleTool));
+//		angleTool += 2.0f * pi / polygon_points;
+//	}
+//	glEnd();
+//
+//	glBegin(GL_POLYGON);
+//	glColor3f(0.0f, 0.0f, 0.0f);
+//	angleTool = 0.0f;
+//	for (GLint i = 0; i < polygon_points; i++)
+//	{
+//		glVertex2d(0.52 + radius_arm * cos(angleTool), 0.0 + radius_arm * sin(angleTool));
+//		angleTool += 2.0f * pi / polygon_points;
+//	}
+//	glEnd();
+//
+//	glPopMatrix();
+//
+//}
 
-	GLfloat angleTool = 0.0f;
+void draw3DScene() {
 
-	// Dibujamos los ejes.
-	glPushMatrix();
-
-	glEnable(GL_LINE_SMOOTH);
-	glBegin(GL_LINES);
-	glLineWidth(1);
-	glColor3f(0.0f, 0.0f, 0.0f);
-
-	glVertex2f(-1.0f, 0.0f);
-	glVertex2f(1.0f, 0.0f);
-
-
-	glVertex2f(0.0f, -1.0f);
-	glVertex2f(0.0f, 1.0f);
-
-	glEnd();
-
-	glPopMatrix();
-
-	// Mano superior.
+	// Mano Superior
 	glPushMatrix();
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	glTranslatef(
-		-(0.52 - cos(toRadians(fAngulo1)) * 0.52) + (cos(toRadians(fAngulo2)) * 0.52), 
-		(sin(toRadians(fAngulo1)) * 0.52) + (sin(toRadians(fAngulo2)) * 0.52), 
-		0.0f);
+	glTranslatef(-0.8f, -0.2f, 0.0f);
+	glRotatef(fAngulo5, 0.0f, 1.f, 0.f);
 
-	glTranslatef(-0.8f + 0.52, -0.2f, 0.0f);	
+	glTranslatef(cos(toRadians(fAngulo1)) * 0.4 + cos(toRadians(fAngulo2)) * 0.4, sin(toRadians(fAngulo1)) * 0.4 + sin(toRadians(fAngulo2)) * 0.4, 0);
 
-	glRotatef(45.0f + fAngulo3 + fAngulo4, 0.0f, 0.0f, 1.0f);
-	glTranslatef(0.0f, 0.01f, 0.0f);
-
-	glBegin(GL_POLYGON);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex2f(0.12f, -0.01f);
-	glVertex2f(0.12f, 0.01f);
-	glVertex2f(0.0f, 0.01f);
-	glVertex2f(0.0f, -0.01f);
-	glEnd();
-
-	glTranslatef(0.12f, 0.0f, 0.0f);
-	glRotatef(-90.0f, 0.0f, 0.0f, 1.0f);
-
-
-	glBegin(GL_POLYGON);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex2f(0.12f, -0.01f);
-	glVertex2f(0.12f, 0.01f);
-	glVertex2f(0.0f, 0.01f);
-	glVertex2f(0.0f, -0.01f);
-	glEnd();
-
-	glBegin(GL_POLYGON);
 	glColor3f(0.0f, 0.0f, 0.0f);
-	angleTool = 0.0f;
-	for (GLint i = 0; i < polygon_points; i++)
-	{
-		glVertex2d(radius_hand * cos(angleTool), radius_hand * sin(angleTool));
-		angleTool += 2.0f * pi / polygon_points;
-	}
-	glEnd();
+	glutWireSphere(radius_arm, 50, 50);
 
-	glBegin(GL_POLYGON);
+	glRotatef(45 + fAngulo3 + fAngulo4, 0.0f, 0.0f, 1.0f);
+	glTranslatef(0.0f, 0.013f, 0.0f);
+	glRotatef(90, 0.0f, 1.0f, 0.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glutWireCylinder(radius_hand, 0.2f, 50, 50);
+
+	glTranslatef(0.0f, 0.0f, 0.2f);
+
 	glColor3f(0.0f, 0.0f, 0.0f);
-	angleTool = 0.0f;
-	for (GLint i = 0; i < polygon_points; i++)
-	{
-		glVertex2d(0.12f + radius_hand * cos(angleTool), radius_hand * sin(angleTool));
-		angleTool += 2.0f * pi / polygon_points;
-	}
-	glEnd();
+	glutWireSphere(radius_hand, 50, 50);
+
+	glRotatef(90, 1.0f, 0.0f, 0.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glutWireCylinder(radius_hand, 0.2f, 50, 50);
+
+	glTranslatef(0.0f, 0.0f, 0.2f);
+
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glutWireSphere(radius_hand, 50, 50);
+
 
 	glPopMatrix();
 
 	// Mano inferior.
-	glPushMatrix();
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-	glTranslatef(
-		-(0.52 - cos(toRadians(fAngulo1)) * 0.52) + (cos(toRadians(fAngulo2)) * 0.52), 
-		(sin(toRadians(fAngulo1)) * 0.52) + (sin(toRadians(fAngulo2)) * 0.52), 
-		0.0f);
-	
-	glTranslatef(-0.8f + 0.52, -0.2f, 0.0f);
-
-	glRotatef(-45.0f + fAngulo3 - fAngulo4, 0.0f, 0.0f, 1.0f);
-	glTranslatef(0.0f, -0.01f, 0.0f);
-
-	glBegin(GL_POLYGON);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex2f(0.12f, -0.01f);
-	glVertex2f(0.12f, 0.01f);
-	glVertex2f(0.0f, 0.01f);
-	glVertex2f(0.0f, -0.01f);
-	glEnd();
-
-	glTranslatef(0.12f, 0.0f, 0.0f);
-	glRotatef(90.0f, 0.0f, 0.0f, 1.0f);
-
-
-	glBegin(GL_POLYGON);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex2f(0.12f, -0.01f);
-	glVertex2f(0.12f, 0.01f);
-	glVertex2f(0.0f, 0.01f);
-	glVertex2f(0.0f, -0.01f);
-	glEnd();
-
-	glBegin(GL_POLYGON);
-	glColor3f(0.0f, 0.0f, 0.0f);
-	angleTool = 0.0f;
-	for (GLint i = 0; i < polygon_points; i++)
-	{
-		glVertex2d(radius_hand * cos(angleTool), radius_hand * sin(angleTool));
-		angleTool += 2.0f * pi / polygon_points;
-	}
-	glEnd();
-
-	glBegin(GL_POLYGON);
-	glColor3f(0.0f, 0.0f, 0.0f);
-	angleTool = 0.0f;
-	for (GLint i = 0; i < polygon_points; i++)
-	{
-		glVertex2d(0.12f + radius_hand * cos(angleTool), radius_hand * sin(angleTool));
-		angleTool += 2.0f * pi / polygon_points;
-	}
-	glEnd();
-
-	glPopMatrix();
-
-	// Antebrazo.
-	glPushMatrix();
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
-	glTranslatef(-(0.52 - cos(toRadians(fAngulo1)) * 0.52), sin(toRadians(fAngulo1)) * 0.52, 0.0f);
-	
-	glTranslatef(-0.8f + 0.52, -0.2f, 0.0f);
-	glRotatef(fAngulo2, 0.0f, 0.0f, 1.0f);
-
-	glBegin(GL_POLYGON);
-	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex2f(0.52f, -0.025f);
-	glVertex2f(0.52f, 0.025f);
-	glVertex2f(0.0f, 0.025f);
-	glVertex2f(0.0f, -0.025f);
-	glEnd();
-
-	glBegin(GL_POLYGON);
-	glColor3f(0.0f, 0.0f, 0.0f);
-	angleTool = 0.0f;
-	for (GLint i = 0; i < polygon_points; i++)
-	{
-		glVertex2d(0.52 + radius_arm * cos(angleTool), 0.0 + radius_arm * sin(angleTool));
-		angleTool += 2.0f * pi / polygon_points;
-	}
-	glEnd();
-
-	glPopMatrix();
-
-	// Brazo.
 
 	glPushMatrix();
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
 	glTranslatef(-0.8f, -0.2f, 0.0f);
-	glRotatef(fAngulo1, 0.0f, 0.0f, 1.0f);
-	
-	glBegin(GL_POLYGON);
+	glRotatef(fAngulo5, 0.0f, 1.f, 0.f);
+
+	glTranslatef(cos(toRadians(fAngulo1)) * 0.4 + cos(toRadians(fAngulo2)) * 0.4, sin(toRadians(fAngulo1)) * 0.4 + sin(toRadians(fAngulo2)) * 0.4, 0);
+
+
+	glRotatef(-45 + fAngulo3 - fAngulo4, 0.0f, 0.0f, 1.0f);
+	glTranslatef(0.0f, -0.013f, 0.0f);
+	glRotatef(90, 0.0f, 1.0f, 0.0f);
 	glColor3f(1.0f, 0.0f, 0.0f);
-	glVertex2f(0.52f, -0.025f);
-	glVertex2f(0.52f, 0.025f);
-	glVertex2f(0.0f, 0.025f);
-	glVertex2f(0.0f,-0.025f);
-	glEnd();
+	glutWireCylinder(radius_hand, 0.2f, 50, 50);
 
-	glBegin(GL_POLYGON);
-	glColor3f(0.0f, 0.0f, 0.0f);
-	angleTool = 0.0f;
-	for (GLint i = 0; i < polygon_points; i++)
-	{
-		glVertex2d(radius_arm * cos(angleTool), radius_arm * sin(angleTool));
-		angleTool += 2.0f * pi / polygon_points;
-	}
-	glEnd();
+	glTranslatef(0.0f, 0.0f, 0.2f);
 
-	glBegin(GL_POLYGON);
 	glColor3f(0.0f, 0.0f, 0.0f);
-	angleTool = 0.0f;
-	for (GLint i = 0; i < polygon_points; i++)
-	{
-		glVertex2d(0.52 + radius_arm * cos(angleTool), 0.0 + radius_arm * sin(angleTool));
-		angleTool += 2.0f * pi / polygon_points;
-	}
-	glEnd();
+	glutWireSphere(radius_hand, 50, 50);
+
+	glRotatef(-90, 1.0f, 0.0f, 0.0f);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glutWireCylinder(radius_hand, 0.2f, 50, 50);
+
+	glTranslatef(0.0f, 0.0f, 0.2f);
+
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glutWireSphere(radius_hand, 50, 50);
 
 	glPopMatrix();
 
-}
-
-void draw3DScene(){
-
+	// Antebrazo
 	glPushMatrix();
-    glMatrixMode(GL_MODELVIEW);
+	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-	glTranslatef(-(0.4 - cos(toRadians(fAngulo1+25)) * 0.4), sin(toRadians(fAngulo1)) * 0.4, sin(toRadians(25)) * 0.4);
+	glTranslatef(-0.8f, -0.2f, 0.0f);
+	glRotatef(fAngulo5, 0.0f, 1.f, 0.f);
 
-	glTranslatef(-0.8f + 0.4f, -0.2f, 0.0f);
-
-	glRotatef(25,1.0f, 0.0f, 0.0f);
-	glRotatef(90 + 25,0.0f, 1.0f, 0.0f);
-
+	glTranslatef(cos(toRadians(fAngulo1)) * 0.4, sin(toRadians(fAngulo1)) * 0.4, 0);
 	glRotatef(fAngulo2, 0.0f, 0.0f, 1.0f);
+	glRotatef(90, 0.0f, 1.0f, 0.0f);
 
 	glColor3f(1.0f, 0.0f, 0.0f);
 	glutWireCylinder(radius_arm, 0.4f, 50, 50);
 
-    glColor3f(0.0f,0.0f, 0.0f);
-	glutWireSphere(radius_arm,50,50);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glutWireSphere(radius_arm, 50, 50);
 
-    glPopMatrix();
+	glPopMatrix();
 
 	// Brazo
 	glPushMatrix();
-    glMatrixMode(GL_MODELVIEW);
+	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
 	glTranslatef(-0.8f, -0.2f, 0.0f);
-
-	glRotatef(25,1.0f, 0.0f, 0.0f);
-	glRotatef(90 + 25, 0.0f, 1.0f, 0.0f);
+	glRotatef(fAngulo5, 0.0f, 1.f, 0.f);
 
 	glRotatef(fAngulo1, 0.0f, 0.0f, 1.0f);
+	glRotatef(90, 0.0f, 1.0f, 0.0f);
+
 
 	glColor3f(1.0f, 0.0f, 0.0f);
 	glutWireCylinder(radius_arm, 0.4f, 50, 50);
 
-    glColor3f(0.0f,0.0f, 0.0f);
-    glutWireSphere(radius_arm,50,50);
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glutWireSphere(radius_arm, 50, 50);
 
-    glPopMatrix();
+	glPopMatrix();
 
 }
 
 void Display(void)
 {
-    //Activamos buffer de profundidad
-    glEnable(GL_DEPTH_TEST);
+	//Activamos buffer de profundidad
+	glEnable(GL_DEPTH_TEST);
 	// Borramos la escena
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glLoadIdentity();
 
 	//draw2DScene();
-    draw3DScene();
+	draw3DScene();
 
 	glFlush();
 	glutSwapBuffers();
@@ -337,52 +408,66 @@ void MyReshape(GLint width, GLint height)
 void Timer(GLint t) {
 
 	// Control Angulo1
-	 if (fAngulo1 <= -45)
-	 	sentido_horario1 = GL_TRUE;
-	 if (fAngulo1 >= 100)
+	if (fAngulo1 <= -45)
+		sentido_horario1 = GL_TRUE;
+	if (fAngulo1 >= 100)
 		sentido_horario1 = GL_FALSE;
-	 if (sentido_horario1) {
-	 	fAngulo1 += 0.5f;
-	 }
-	 else {
-	 	fAngulo1 -= 0.5f;
-	 }
+	if (sentido_horario1) {
+		fAngulo1 += 0.5f;
+	}
+	else {
+		fAngulo1 -= 0.5f;
+	}
 
 	// Control Angulo2
-	 if (fAngulo2 <= -30)
-	 	sentido_horario2 = GL_TRUE;
-	 if (fAngulo2 >= 60)
-	 	sentido_horario2 = GL_FALSE;
-	 if (sentido_horario2) {
-	 	fAngulo2 += 1.0f;
-	 }
-	 else {
-	 	fAngulo2 -= 1.0f;
-	 }
+	if (fAngulo2 <= -30)
+		sentido_horario2 = GL_TRUE;
+	if (fAngulo2 >= 60)
+		sentido_horario2 = GL_FALSE;
+	if (sentido_horario2) {
+		fAngulo2 += 1.0f;
+	}
+	else {
+		fAngulo2 -= 1.0f;
+	}
 
-	 // Control Angulo3
-	 if (fAngulo3 <= -45)
-	 	sentido_horario3 = GL_TRUE;
-	 if (fAngulo3 >= 45)
-	 	sentido_horario3 = GL_FALSE;
-	 if (sentido_horario3) {
-	 	fAngulo3 += 0.5f;
-	 }
-	 else {
-	 	fAngulo3 -= 0.5f;
-	 }
+	// Control Angulo3
+	if (fAngulo3 <= -45)
+		sentido_horario3 = GL_TRUE;
+	if (fAngulo3 >= 45)
+		sentido_horario3 = GL_FALSE;
+	if (sentido_horario3) {
+		fAngulo3 += 0.5f;
+	}
+	else {
+		fAngulo3 -= 0.5f;
+	}
 
-	 // Control Angulo4
-	 if (fAngulo4 <= 0)
-	 	sentido_horario4 = GL_TRUE;
-	 if (fAngulo4 >= 45)
-	 	sentido_horario4 = GL_FALSE;
-	 if (sentido_horario4) {
-	 	fAngulo4 += 2.0f;
-	 }
-	 else {
-	 	fAngulo4 -= 2.0f;
-	 }
+	// Control Angulo4
+	if ((GetKeyState(VK_LBUTTON) & 0x8000) != 0) {
+		if (fAngulo4 < 45)
+		{
+			fAngulo4 += 2.0f;
+		}
+	}
+	else {
+		if (fAngulo4 > 0)
+		{
+			fAngulo4 -= 2.0f;
+		}
+	}
+
+	// Control Angulo5
+	if (fAngulo5 <= -90)
+		sentido_horario5 = GL_TRUE;
+	if (fAngulo5 >= 90)
+		sentido_horario5 = GL_FALSE;
+	if (sentido_horario5) {
+		fAngulo5 += 0.5f;
+	}
+	else {
+		fAngulo5 -= 0.5f;
+	}
 
 	glutPostRedisplay();
 	glutTimerFunc(tiempo, Timer, 0.0f);
@@ -404,7 +489,7 @@ int main(int argc, char** argv)
 
 	// Indicamos cuales son las funciones de redibujado e idle
 	glutDisplayFunc(Display);
-	//glutTimerFunc(tiempo, Timer, 0.0f);
+	glutTimerFunc(tiempo, Timer, 0.0f);
 
 	//Ajuste de proporciones
 	glutReshapeFunc(MyReshape);
