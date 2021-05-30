c019625301da554c67cea1e40330bd7d4a4bf32f
Felix Aguilar Ferrer <aguilarferrerfelix@gmail.com>
2021-05-30 17:37:21 +0200

    Add files via upload

--- Etapa 6/Etapa_6.cpp
+++ Etapa 6/Etapa_6.cpp
@@ -771,12 +771,16 @@ void key_set(unsigned char key, int x , int y) {
 		tilt_vector[0] -= 0.01; // Tiltea la camara a la derecha
 		break;
 	}
+	GLfloat aux = sin(toRadians(fAngulo1)) * (0.4 + desp) + sin(toRadians(fAngulo2)) * (0.4 + desp * 2 + lextension);
 
-
-
-	if (lcuerda - (sin(toRadians(fAngulo1)) * (0.4 + desp) + sin(toRadians(fAngulo2)) * (0.4 + desp * 2 + lextension)) > 0.70) {
-		lcuerda = (sin(toRadians(fAngulo1)) * (0.4 + desp) + sin(toRadians(fAngulo2)) * (0.4 + desp * 2 + lextension)) + 0.70;
+	if ((lcuerda - aux) > 0.70 && lcuerda > 0.2) {
+		lcuerda = aux + 0.70;
 	}
+
+	//while ((lcuerda - aux) > 0.70 && lextension != 0) {
+	//	lextension -= 0.01;
+	//	aux = sin(toRadians(fAngulo1)) * (0.4 + desp) + sin(toRadians(fAngulo2)) * (0.4 + desp * 2 + lextension);
+	//}
 }
 
 void Timer(GLint t)
